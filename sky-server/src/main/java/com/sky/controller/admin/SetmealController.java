efd243c438ab4e8ca42dc29153b0e109343f58dc
FreeLife <3346114494@qq.com>
2026-01-17 15:16:24 +0800

    套餐分页查询业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -2,17 +2,18 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 套餐管理
  */
+@Slf4j
 @RestController
 @RequestMapping("/admin/setmeal")
 public class SetmealController {
@@ -27,7 +28,23 @@ public class SetmealController {
      */
     @PostMapping
     public Result save(@RequestBody SetmealDTO setmealDTO) {
+        log.info("新增套餐:{}", setmealDTO);
         setmealService.saveWithDish(setmealDTO);
         return  Result.success();
+    }
+
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<PageResult> page( SetmealPageQueryDTO setmealPageQueryDTO){
+        log.info("套餐分页查询：{}", setmealPageQueryDTO);
+
+        PageResult pageResult = setmealService.setmaelQuery(setmealPageQueryDTO);
+
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotaion.AutoFill;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -27,4 +30,11 @@ public interface SetmealMapper {
             "(#{categoryId},#{name}, #{price}, #{status}, #{description}, #{image}, #{createTime},#{updateTime}, #{createUser}, #{updateUser})")
     @AutoFill(value = OperationType.INSERT)
     void insert(Setmeal setmeal);
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    Page<SetmealVO> setmealQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
