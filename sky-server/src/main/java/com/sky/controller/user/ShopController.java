3a387d5c61859c5c899182084817074df08892b7
FreeLife <3346114494@qq.com>
2026-01-23 21:30:23 +0800

    店铺营业状态设置业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.user;
+
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 店铺相关接口
+ */
+@Slf4j
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    
+    /**
+     * 获取店铺的营业状态
+     * @return
+     */
+    @GetMapping("/status")
+     public Result<Integer> getStatus(){
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get(KEY);
+
+        log.info("获取到店铺的营业状态为：{}", shopStatus == 1 ? "营业中" : "打烊中");
+        return Result.success(shopStatus);
+
+    }
+}
